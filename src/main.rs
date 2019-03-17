5a91ee66132e16b1c5d8ef10f0988afe7ce1458e
Thomas Campistron <irevoire@hotmail.fr>
2019-03-17 21:06:02 +0100

    rustfmt my file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use reqwest;
 
 use select::document::Document;
@@ -14,8 +13,6 @@ const MENU: &str = "/WidgetPage.aspx?widgetId=35";
 const COOKIE: &str = "portal_url=restaurant-seclin.atosworldline.com/; language=FR";
 
 fn main() {
-    let today = Local::now();
-
     let resp = reqwest::Client::new()
         .get(&format!("{}{}", BASE_URL, MENU))
         .header("Cookie", COOKIE) // needed to avoid redirection
@@ -30,7 +27,8 @@ fn main() {
         .collect();
 
     // get all the dates
-    let timestamps: Vec<i64> = list.iter()
+    let timestamps: Vec<i64> = list
+        .iter()
         .map(|path| path.split("/").last().unwrap())
         .map(|file| compute_file(file).unwrap())
         .collect();
@@ -40,25 +38,25 @@ fn main() {
     let index = timestamps.iter().position(|v| *v == *value).unwrap();
     let current_menu = format!("{}{}", BASE_URL, list[index]);
     println!("This week link {}", current_menu);
-
 }
 
 fn compute_file(file: &str) -> Option<i64> {
     let today = Local::now();
 
     let re = Regex::new(r"(?i)semaine du (?P<start>\d{2}) au (?P<end>\d{2})").unwrap();
-   // let dates = Vec::new();
+    // let dates = Vec::new();
     for group in re.captures_iter(file) {
         let start = group["start"].parse().unwrap();
         let end = group["end"].parse().unwrap();
 
         // if end is < start then we are the next month
         let end = if end < start {
-            Utc.ymd(today.year(), today.month() + 1, end).and_hms(0,0,0)
+            Utc.ymd(today.year(), today.month() + 1, end)
+                .and_hms(0, 0, 0)
         } else {
-            Utc.ymd(today.year(), today.month(), end).and_hms(0,0,0)
+            Utc.ymd(today.year(), today.month(), end).and_hms(0, 0, 0)
         };
-        let start = Utc.ymd(today.year(), today.month(), start).and_hms(0,0,0);
+        let start = Utc.ymd(today.year(), today.month(), start).and_hms(0, 0, 0);
 
         // then convert the date to EPOCH to do the mean
         let start = start.timestamp();
