fd39bfa3109e44e1025d29d9eb74900a80e62a53
Thomas Campistron <irevoire@hotmail.fr>
2019-03-17 22:05:38 +0100

    now I get the file. I need ot convert it to html

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+
+use regex::Regex;
+
+use chrono::prelude::*;
+use chrono::TimeZone;
+
+pub fn from_xls(file: File) {
+    println!("Convert xls to html");
+}
+
+pub fn compute_file(file: &str) -> Option<i64> {
+    let today = Local::now();
+
+    let re = Regex::new(r"(?i)semaine du (?P<start>\d{2}) au (?P<end>\d{2})").unwrap();
+    // let dates = Vec::new();
+    for group in re.captures_iter(file) {
+        let start = group["start"].parse().unwrap();
+        let end = group["end"].parse().unwrap();
+
+        // if end is < start then we are the next month
+        let end = if end < start {
+            Utc.ymd(today.year(), today.month() + 1, end)
+                .and_hms(0, 0, 0)
+        } else {
+            Utc.ymd(today.year(), today.month(), end).and_hms(0, 0, 0)
+        };
+        let start = Utc.ymd(today.year(), today.month(), start).and_hms(0, 0, 0);
+
+        // then convert the date to EPOCH to do the mean
+        let start = start.timestamp();
+        let end = end.timestamp();
+
+        let mean = (start + end) / 2;
+
+        return Some((today.timestamp() - mean).abs()); // we know we have only one group
+    }
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ use reqwest;
 use select::document::Document;
 use select::predicate::Name;
 
-use regex::Regex;
+use std::fs::File;
 
-use chrono::prelude::*;
-use chrono::TimeZone;
+mod lib;
 
 const BASE_URL: &str = "http://restaurant-seclin.atosworldline.com";
 const MENU: &str = "/WidgetPage.aspx?widgetId=35";
@@ -30,41 +29,23 @@ fn main() {
     let timestamps: Vec<i64> = list
         .iter()
         .map(|path| path.split("/").last().unwrap())
-        .map(|file| compute_file(file).unwrap())
+        .map(|file| lib::compute_file(file).unwrap())
         .collect();
 
     // get the index of the closest week of today
     let value = timestamps.iter().min().unwrap();
     let index = timestamps.iter().position(|v| *v == *value).unwrap();
     let current_menu = format!("{}{}", BASE_URL, list[index]);
-    println!("This week link {}", current_menu);
-}
 
-fn compute_file(file: &str) -> Option<i64> {
-    let today = Local::now();
+    let mut xls = reqwest::Client::new()
+        .get(&current_menu)
+        .header("Cookie", COOKIE) // needed to avoid redirection
+        .send()
+        .unwrap();
 
-    let re = Regex::new(r"(?i)semaine du (?P<start>\d{2}) au (?P<end>\d{2})").unwrap();
-    // let dates = Vec::new();
-    for group in re.captures_iter(file) {
-        let start = group["start"].parse().unwrap();
-        let end = group["end"].parse().unwrap();
+    // TODO randomize filename
+    let mut path = File::create("/tmp/crustyline.xls").unwrap();
+    xls.copy_to(&mut path).unwrap();
 
-        // if end is < start then we are the next month
-        let end = if end < start {
-            Utc.ymd(today.year(), today.month() + 1, end)
-                .and_hms(0, 0, 0)
-        } else {
-            Utc.ymd(today.year(), today.month(), end).and_hms(0, 0, 0)
-        };
-        let start = Utc.ymd(today.year(), today.month(), start).and_hms(0, 0, 0);
-
-        // then convert the date to EPOCH to do the mean
-        let start = start.timestamp();
-        let end = end.timestamp();
-
-        let mean = (start + end) / 2;
-
-        return Some((today.timestamp() - mean).abs()); // we know we have only one group
-    }
-    None
+    lib::from_xls(path);
 }
